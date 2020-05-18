f91ef1aa36f867216e3e97ba284084ebb9bc1c92
krousk <65219362+krousk@users.noreply.github.com>
2020-05-18 23:27:04 +0900

    Initial commit

---
+++ Nam Kyeong Hun_20.05.15.R
@@ -0,0 +1,28 @@
+#남경훈 20.05.14/20.05.15
+
+#문1
+matrix(1:70, nrow = 10, ncol = 2, byrow=T)
+
+#문2
+state.x77
+st <- data.frame(state.x77)
+str(st)
+tail(st)
+head(st)
+dim(st)
+class(st)
+summary(st)
+rowSums(st)
+rowMeans(st)
+colSums(st)
+colMeans(st)
+str(st[9,])
+
+
+
+#문3
+mtcars
+
+class(mtcars)
+dim(mtcars)
+
