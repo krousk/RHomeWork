9e0bd0beb8aa11484a9feb282c839dfd80e461d1
krousk <krousk@naver.com>
2020-05-21 17:51:36 +0900

    21end

--- Nam Kyeong Hun_20.05.19.R
+++ Nam Kyeong Hun_20.05.19.R
@@ -2,13 +2,13 @@
 
 #문제1
 #
-#1
+#1 변수 하나, 값의 형태는 범주형. 남,여 두 종류 뿐이니까.
 gender <- c('F','F','F','M','M','F','F','F','M','M')
 class(gender)
 dim(gender)
 str(gender)
 
-#2
+#2            해당하는 범주의 빈도수.          cg까지 여기서 적었어야 함.
 table(gender)
 
 #3
@@ -19,9 +19,9 @@ class(cg)
 dim(cg)
 str(cg)
 
-barplot(cg, main="class gender")
+barplot(cg, main="class gender")   #* xlab, ylab 빠짐.  히스토그램에서 x는 구간, 막대그래프에서는
 
-#4
+#4 비율을 표현할때는 파이가 더 유리.
 pie(cg, main = "class gender")
 
 #문제2
@@ -34,7 +34,7 @@ class(season)
 dim(season)
 str(season)
 
-#2
+#2  이거도 fv를 여기에다 적었어야 했음.
 table(season)
 
 #3
@@ -57,7 +57,7 @@ pie(fv.array, main = "four season vote")
 
 #문제3
 #
-#1*
+#1* names 함수 까먹었음.
 score <-c(90, 85, 73, 80, 85, 65,78,50,68,96)
 score
 
@@ -68,26 +68,26 @@ score
 score
 
 #3
-mean(score)
-median(score)
+mean(score)  #데이터 대표는 가능하지만 이상치에 영향을 받음
+median(score) #그래서 중앙값 이용.
 
-#4
+#4 표준편차가 크다는 것은 값이 들쓕날쑥하다는 뜻이다. 
 sd(score)
 
-#5*
+#5* 힌트: names, max, ==
 which.max(score)
 
-#6
+#6  면적이 중요함. 1사분위 쪽에 모여있다.
 boxplot(score)
-boxplot(score)$out #*
+boxplot(score)$out #* 이상치 없으면 그냥 없다고 하면 됨.
 
-#7*
+#7* 이건 또 왜 이렇게 복잡하게 했다냐... 구간별로빈도수 표현하는 게 히스토그램. 전체적인 데이터의 구간별 분포가 고르냐 치우쳐있느냐를 확인. 
 hist(score, main="학생 성적",xlab="점수", ylab="빈도수", col="purple", las=1, breaks=5)
 
 #
 #
-#문제4*
-#
+#문제4* 그냥 다 틀렸으니 처음부터 다시 하자.       #일정한 형태로 구성이 된다면 범주형으로 간주.
+#head, tail 적용하는 거 다시 생각할 것.
 mtcars
 head(wt)
 wt <- c(mtcars[,6])
@@ -95,14 +95,18 @@ wt
 
 cyl <- c(mtcars)
 cyl
-#1
+#1 여긴 맞았는데.... 모양이 다름.   분포가 비슷. 특정한 방향으로 치우친 데이는 아님. 
 mean(wt)
 median(wt)
 mean(wt, trim = 0.15)
 sd(wt)
 
-#2
+#2 이것도 값은 맞긴 한데... 다시할 것. 4분위수, 최소최대값 등을 구할 때 사용.(2사분위-중앙값)
 summary(wt)
 
-#3
+#3 여기서부터 막힘. $~~ 하는 거가 문제. 실린더 수는 범주형으로 간주. 그래서 도수분포표, 막대, 원형그래프 등등 가능.
 table(cyl)
+
+
+#문제에서, 데이터가 고르게 분포되지는 않음. 종모양이어야 고른 것. 
+#그룹으로 그래프를 보겠다는 건 서로간의 형태를 보겠다는 것. 8번 문제.
--- Nam Kyeong Hun_20.05.20.R
+++ Nam Kyeong Hun_20.05.20.R
@@ -13,7 +13,7 @@ abline(Q1)
 
 cor(income, edu.time)
 
-#수익과 교육기간 사이에는 약 0.79의 상관계수가 있는 것으로 나타났다.
+#수익과 교육기간 사이에는 약 0.79의 상관계수가 있는 것으로 나타났다. 양의 상관관계
 #이를 바탕으로 볼 때, 교육기간이 길어질 수록 수익 역시 증가한다고 볼 수 있다.
 
 #문제2
@@ -30,11 +30,12 @@ Q2 <- lm(grade~viewtime, data = grti)
 abline(Q2)
 
 cor(grade,viewtime)
-#성적과 시청시간 사이에는 약 -0.62의 상관계수가 있는 것으로 나타났다.
+#성적과 시청시간 사이에는 약 -0.62의 상관계수가 있는 것으로 나타났다. 음의 상관관계
 #따라서, 시청시간이 길어질수록 성적이 떨어진다고 볼 수 있다.
 
-#문제3
+#문제3* 답은 맞다.
 mtcars
+head(mtcars)  #*
 
 vs <- c("mpg","cyl", "disp","hp", "drat", "wt", "qsec","vs", "am", "gear","carb")
 vs
@@ -55,11 +56,14 @@ pop
 plot(year, pop, main="예상 인구 수 추계")
 
 
-#문제5
+#문제5*
 library(rJava)
 library(xlsx)
 
-act <- read.csv('C:\\Rworkspace\\new-R-working-space\\도로교통공단_시도_시군구별_월별_교통사고(2018).csv')
+#*데이터 분석을 어떻게, 무슨 방향으로 할 것인지 전략구축.
+setwd("C:\\R working space\\")
+act <- read.csv(file= 'C:\\R working space\\도로교통공단_시도_시군구별_월별_교통사고(2018).csv',
+                header=T)
 act
 #문제 정의
 #각 지역별 월별 발생건수, 사망자수, 부상자수를 취합한다.
@@ -69,3 +73,52 @@ tail(act)
 class(act)
 dim(act)
 str(act)
+
+act$시도 <- as.character(act$시도) #양이 많아 쉽지가 않으니 분할.
+act$시도
+act$시군구 <- as.character(act$시군구)
+act$시군구
+act$월 <-act$월
+act$월
+
+month <- c()
+for ( i in 1:nrow( act ) ) {
+    if ( act[ i, "월" ] == "01월" ) {
+        month[ i ] <- 1
+    } else if ( act[ i, "월" ] == "02월" ) {
+        month[ i ] <- 2
+    } else if ( act[ i, "월" ] == "03월" ) {
+        month[ i ] <- 3
+    } else if (  act[ i, "월" ] == "04월" ) {
+        month[ i ] <- 4
+    } else if ( act[ i, "월" ] == "05월" ) {
+        month[ i ] <- 5
+    } else if ( act[ i, "월" ] == "06월" ) {
+        month[ i ] <- 6
+    } else if ( act[ i, "월" ] == "07월" ) {
+        month[ i ] <- 7
+    } else if ( act[ i, "월" ] == "08월" ) {
+        month[ i ] <- 8
+    } else if ( act[ i, "월" ] == "09월" ) {
+        month[ i ] <- 9
+    } else if ( act[ i, "월" ] == "10월" ) {
+        month[ i ] <- 10
+    } else if ( act[ i, "월" ] == "11월" ) {
+        month[ i ] <- 11
+    } else if ( act[ i, "월" ] == "12월" ) {
+        month[ i ] <- 12
+    }  
+}
+month
+    
+act <- data.frame(act,month)
+act
+head(act)
+tail(act)
+
+local <- unique(act[,1])  #unique 함수
+local
+
+local.seoul <- act[act$시도==local[1],]
+local.seoul
+dim(local.seoul)
